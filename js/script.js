1bc97790796e2ca8af0927eb057e0de82f0c91b2
Andrew Todd <drew.todd@gmail.com>
2015-02-19 11:20:18 -0800

    Cleaned up code
    Removed a bunch of commented stuff that I was trying out, now that I
    have a working copy.

--- js/script.js
+++ js/script.js
@@ -5,8 +5,6 @@ $("document").ready(function(){
 		function() { $("#status").css('visibility', 'hidden'); }
 	);
 
-	// $(".box").hover(showStatus, hideStatus);
-
 	$(".box").click(function() {
 		if ($("#toggle-sync").prop("checked")) { // see whether the checkbox is checked or not
 			toggleBoxes();
@@ -15,10 +13,8 @@ $("document").ready(function(){
 		}
 	});
 
-	//$(".box").hover(showStatus,hideStatus);
-
 	function toggleBoxes() {
-		$(".box").toggleClass("red").toggleClass("green"); // statement chaining FTW!
+		$(".box").toggleClass("red").toggleClass("green");
 
 		$("div .red").text("I am a red box.");
 		$("div .green").text("I am a green box.");
@@ -37,17 +33,4 @@ $("document").ready(function(){
 
 		return true;
 	}
-
-	// function showStatus() {
-	// 	//$("div .status").show().fadeIn();
-	// 	$("#status").css('visibility', 'visible');
-	// 	// console.log("bam!");
-	// 	//$("#status").toggle(100);
-	// }
-
-	// function hideStatus() {
-	// 	//$("div .status").hide().fadeOut();
-	// 	$("#status").css('visibility', 'hidden');
-	// 	//$("#status").toggle(100);
-	// }
 });
